45a06120deffc7b05b195c43c7de1fe16c5533ce
shkarovy <y.shkarovetskade@gmail.com>
2025-03-31 21:29:48 +0200

    1.py

--- Lesson 2/1.py
+++ Lesson 2/1.py
@@ -1,4 +1,11 @@
-variable = int(input("Enter number vor 4 Zahl pleas: "))
+number = int(input("Enter number vor 4 Zahl pleas: "))
 
-toulsend =
-print(variable)
+thousands = number // 1000
+hundreds = (number % 1000) // 100
+tens = (number % 100) // 10
+ones = number % 10
+
+print(thousands)
+print(hundreds)
+print(tens)
+print(ones)
