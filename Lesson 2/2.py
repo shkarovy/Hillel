75b85c05cac17e3819d04760255ce9ff3a142570
shkarovy <y.shkarovetskade@gmail.com>
2025-03-31 21:30:30 +0200

    2.py

---
+++ Lesson 2/2.py
@@ -0,0 +1,12 @@
+number = int(input("Enter number vor 4 Zahl pleas: "))
+
+thousands = number // 1000
+hundreds = (number % 1000) // 100
+tens = (number % 100) // 10
+ones = number % 10
+
+print(ones)
+print(tens)
+print(hundreds)
+print(thousands)
+
