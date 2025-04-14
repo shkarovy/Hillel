2302bd66863097f5c6ec0f5ad7512019c734e33b
Scool_Hillel <y.shkarovetskade@gmail.com>
2025-04-14 21:30:54 +0200

    New Script

--- Lesson 3/1.py
+++ Lesson 3/1.py
@@ -1,2 +1,10 @@
-print(5 > 2)
+num1 = float(input("Enter the first number: "))
 
+operation = input("Enter an operation (+, -, *, /): ")
+
+num2 = float(input("Enter the second number: "))
+
+if operator == "+"
+    print(num1 + num2)
+else operator == "-"
+    print(num1 - num2)
