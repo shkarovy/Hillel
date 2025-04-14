c8383259a7eca289ccd6c92297b25201da993123
Scool_Hillel <y.shkarovetskade@gmail.com>
2025-04-14 21:37:33 +0200

    New Script

--- Lesson 3/1.py
+++ Lesson 3/1.py
@@ -1,10 +1,17 @@
 num1 = float(input("Enter the first number: "))
-
 operation = input("Enter an operation (+, -, *, /): ")
-
 num2 = float(input("Enter the second number: "))
-
-if operator == "+"
+if operation == "+":
     print(num1 + num2)
-else operator == "-"
+elif operation == "-":
     print(num1 - num2)
+elif operation == "*":
+    print(num1 * num2)
+elif operation == "/":
+    if num2 == 0:
+        print("Error: Division by zero is not allowed!")
+    else:
+        result = num1 / num2
+        print("Result:", result)
+else:
+    print("Unknown operation. Please enter one of the following: +, -, *, /")
